57988182fbeb26ade6acce7df77e22916a0fc38f
Shruti Gupta <82036296+shr-sy@users.noreply.github.com>
2025-07-30 11:36:01 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -85,7 +85,7 @@ resource "azurerm_key_vault" "kv" {
     tenant_id = data.azurerm_client_config.current.tenant_id
     object_id = data.azurerm_client_config.current.object_id
 
-    secret_permissions = ["get", "set", "list", "delete"]
+    secret_permissions = ["Get", "Set", "List", "Delete"]
   }
 }
 
