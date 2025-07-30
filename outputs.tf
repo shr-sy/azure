5862e6b273ef14f892863cbd657e56e444de7fee
Shruti Gupta <82036296+shr-sy@users.noreply.github.com>
2025-07-30 11:27:55 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,15 +1,19 @@
 output "app_service_url" {
-  value = azurerm_linux_web_app.app.default_hostname
+  description = "The default hostname of the Linux App Service"
+  value       = azurerm_linux_web_app.app.default_hostname
 }
 
 output "function_app_url" {
-  value = azurerm_linux_function_app.function.default_hostname
+  description = "The default hostname of the Azure Function App"
+  value       = azurerm_linux_function_app.function.default_hostname
 }
 
 output "sql_server_name" {
-  value = azurerm_mssql_server.sql_server.name
+  description = "The name of the Azure SQL Server"
+  value       = azurerm_mssql_server.sql_server.name
 }
 
 output "key_vault_uri" {
-  value = azurerm_key_vault.kv.vault_uri
+  description = "The URI of the Azure Key Vault"
+  value       = azurerm_key_vault.kv.vault_uri
 }
